683a51700b7b60e31e5f83b296792fd003b52c66
DAKSHKT9 <dakshkatkar89@gmail.com>
2026-03-23 15:13:05 +0530

    added structure.txt

--- models/patients.js
+++ models/patients.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const patientSchema = new mongoose.Schema(
   {
-    patientId: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
     gender: {
       type: String,
